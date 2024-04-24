371cedc53d3f1c8dddb37c28257b3dfe20b61e26
Danny Cao <dannycao1997@yahoo.com>
2024-04-23 21:27:04 -0400

    BCryptPasswordEncoder Bean

--- src/main/java/com/fishinghub/fishinghub/config/SecurityConfig.java
+++ src/main/java/com/fishinghub/fishinghub/config/SecurityConfig.java
@@ -1,7 +1,14 @@
 package com.fishinghub.fishinghub.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
