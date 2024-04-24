013e183e640790fc1248bac0fd15f8c2ebf9cf36
Danny Cao <dannycao1997@yahoo.com>
2024-04-23 21:08:18 -0400

    CRUD OPS for Service Classes

--- src/main/java/com/fishinghub/fishinghub/service/CatchService.java
+++ src/main/java/com/fishinghub/fishinghub/service/CatchService.java
@@ -1,4 +1,34 @@
 package com.fishinghub.fishinghub.service;
+import com.fishinghub.fishinghub.entity.Catch;
+import com.fishinghub.fishinghub.repository.CatchRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class CatchService {
+
+    @Autowired
+    private CatchRepository catchRepository;
+
+    public Catch logCatch(Catch catchRecord) {
+        return catchRepository.save(catchRecord);
+    }
+
+    public List<Catch> getAllCatches() {
+        return catchRepository.findAll();
+    }
+
+    public Catch getCatchById(Long id) {
+        return catchRepository.findById(id).orElseThrow(() -> new RuntimeException("Yo Catch? can't find it"));
+    }
+
+    public Catch updateCatch(Catch catchRecord) {
+        return catchRepository.save(catchRecord);
+    }
+
+    public void deleteCatch(Long id) {
+        catchRepository.deleteById(id);
+    }
 }
--- src/main/java/com/fishinghub/fishinghub/service/PostService.java
+++ src/main/java/com/fishinghub/fishinghub/service/PostService.java
@@ -1,4 +1,34 @@
 package com.fishinghub.fishinghub.service;
+import com.fishinghub.fishinghub.entity.Post;
+import com.fishinghub.fishinghub.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    public Post createPost(Post post) {
+        return postRepository.save(post);
+    }
+
+    public List<Post> getAllPosts() {
+        return postRepository.findAll();
+    }
+
+    public Post getPostById(Long id) {
+        return postRepository.findById(id).orElseThrow(() -> new RuntimeException("Can't find yo post"));
+    }
+
+    public Post updatePost(Post post) {
+        return postRepository.save(post);
+    }
+
+    public void deletePost(Long id) {
+        postRepository.deleteById(id);
+    }
 }
