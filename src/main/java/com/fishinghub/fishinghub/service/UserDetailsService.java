a04ef6646579155e99b7b96b34694aaad79ddef7
Danny Cao <dannycao1997@yahoo.com>
2024-04-23 22:28:59 -0400

    attempting to create tests

--- src/main/java/com/fishinghub/fishinghub/service/UserDetailsService.java
+++ src/main/java/com/fishinghub/fishinghub/service/UserDetailsService.java
@@ -4,14 +4,13 @@ import com.fishinghub.fishinghub.entity.User;
 import com.fishinghub.fishinghub.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 
 @Service
-public class CustomUserDetailsService implements UserDetailsService {
+public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
     @Autowired
     private UserRepository userRepository;
 
