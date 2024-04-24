e3c1c492e52fa930fc32f07e87179b17819ac5b7
Danny Cao <dannycao1997@yahoo.com>
2024-04-23 21:06:24 -0400

    experimenting with CRUD OPS for UserService :O

--- src/main/java/com/fishinghub/fishinghub/service/UserService.java
+++ src/main/java/com/fishinghub/fishinghub/service/UserService.java
@@ -1,4 +1,46 @@
 package com.fishinghub.fishinghub.service;
+import com.fishinghub.fishinghub.entity.User;
+import com.fishinghub.fishinghub.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import java.util.List;
 
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public User registerUser(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    public User validateUser(String username, String password) {
+        User user = userRepository.findByUsername(username).orElse(null);
+        if (user != null && bCryptPasswordEncoder.matches(password, user.getPassword())) {
+            return user;
+        }
+        return null;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new RuntimeException("Wyd? can't find u"));
+    }
+
+    public User updateUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
 }
