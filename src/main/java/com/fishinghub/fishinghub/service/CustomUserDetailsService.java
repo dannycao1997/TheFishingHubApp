74985c3972f6f5400e304cfc1788b3c6e6eb8514
Danny Cao <dannycao1997@yahoo.com>
2024-04-23 22:09:49 -0400

    implenting userdetailservice

---
+++ src/main/java/com/fishinghub/fishinghub/service/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.fishinghub.fishinghub.service;
+
+import com.fishinghub.fishinghub.entity.User;
+import com.fishinghub.fishinghub.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), new ArrayList<>());
+    }
+}
