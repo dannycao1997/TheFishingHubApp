40c5543eebba9e462b907e2e380ee195d4e6d365
Danny Cao <dannycao1997@yahoo.com>
2024-04-23 21:57:56 -0400

    debugging security config

--- src/main/java/com/fishinghub/fishinghub/config/SecurityConfig.java
+++ src/main/java/com/fishinghub/fishinghub/config/SecurityConfig.java
@@ -1,5 +1,4 @@
 package com.fishinghub.fishinghub.config;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,8 +6,10 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -30,5 +31,14 @@ public class SecurityConfig {
         return authProvider;
     }
 
-
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable) // disabling csrf protection
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/api/users/signup", "/api/users/login").permitAll()
+                        .anyRequest().authenticated()) // requiring authentication for all other requestss
+                .httpBasic(httpBasic -> {}) //enabling basic http authentication
+                .build();
+    }
 }
